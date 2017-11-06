d28bb231f1b386f631ead99d4e50e54b4d3a3b7d
Shraddha Apharande <Shraddha Apharande>
2017-11-06 12:06:17 -0800

    Initial Commit with package structure and input variables initialization

---
+++ FizzBuzz.java
@@ -0,0 +1,29 @@
+package kenzan.java8.solution;
+
+import java.util.Scanner;
+
+public class FizzBuzz {
+
+
+	public static void main(String[] a) {
+		
+		System.out.println("Enter FizzBuzz upper boundry:");
+		
+		Scanner scanner = new Scanner(System.in);
+			
+			int input = scanner.nextInt();
+
+			StringBuilder fizzbuzz = new StringBuilder("fizzbuzz:");
+			StringBuilder fizz = new StringBuilder("fizz:");
+			StringBuilder buzz = new StringBuilder("buzz:");
+			StringBuilder others = new StringBuilder("others:");
+
+			//To Do : Logic in Java 8
+			
+			
+		} 
+	}
+
+	
+	
+
