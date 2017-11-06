e366ef07f2a75f10503cb23dc42dc739280c4d7a
Shraddha Apharande <Shraddha Apharande>
2017-11-06 14:52:20 -0800

    Final Version Commit

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -6,36 +6,53 @@ import static java.util.stream.Collectors.joining;
 
 public class FizzBuzz {
 
-
 	public static void main(String[] a) {
-		
-		System.out.println("Enter FizzBuzz upper boundry:");
-		
-		try (Scanner scanner = new Scanner(System.in)) {
-			
-			int input = scanner.nextInt();
 
-			StringBuilder fizzbuzz = new StringBuilder("fizzbuzz:");
-			StringBuilder fizz = new StringBuilder("fizz:");
-			StringBuilder buzz = new StringBuilder("buzz:");
-			StringBuilder others = new StringBuilder();
-
-			IntStream.rangeClosed(1, input).parallel()
-			.mapToObj(i -> i % (5 * 3) == 0 ? fizzbuzz.append(i + ",")
-					: i % 3 == 0 ? fizz.append(i + ",")
-							: i % 5 == 0 ? buzz.append(i + ",") : others.append(i + ","))
-			.collect(joining(System.lineSeparator()));
-			System.out.println("FizzBuzz Results");
-			System.out.println("==============================");
-			
-			System.out.println(fizz + "\n" + buzz + "\n" + fizzbuzz);
-			
-			
-		} catch (Exception e) {
-			System.out.println("Invalid input.Please enter numeric upper boundary");
-		}
-	}
-
-}	
+		String ans = "Yes";
+		int input;
+		Scanner scanner = null;
 	
+		do {
+			try {
+				System.out.println("Enter FizzBuzz upper boundry:");
+				scanner = new Scanner(System.in);
 
+				input = Integer.parseInt(scanner.nextLine());
+
+				StringBuilder fizzbuzz = new StringBuilder("fizzbuzz:");
+				StringBuilder fizz = new StringBuilder("fizz:");
+				StringBuilder buzz = new StringBuilder("buzz:");
+				StringBuilder others = new StringBuilder();
+
+				//Logic of FizzBuzz
+				IntStream.rangeClosed(1, input).parallel()
+						.mapToObj(i -> i % (5 * 3) == 0 ? fizzbuzz.append(i + ",")
+								: i % 3 == 0 ? fizz.append(i + ",")
+										: i % 5 == 0 ? buzz.append(i + ",") : others.append(i + ","))
+						.collect(joining(System.lineSeparator()));
+				
+				//Print Result
+				System.out.println("FizzBuzz Results");
+				System.out.println("==============================");
+
+				System.out.println(fizz + "\n" + buzz + "\n" + fizzbuzz);
+
+				System.out.println("Do you want to continue?(Yes/No)");
+				ans = scanner.nextLine();
+
+			} catch (Exception e) {
+				System.out.println("Invalid input.Upper boundry should be numeric value");
+				if (scanner != null) {
+					System.out.println("Do you want to continue?(Yes/No)");
+					ans = scanner.nextLine();
+				}
+			}finally{
+				if(!ans.equalsIgnoreCase("yes")){
+					scanner.close();
+				}
+			}
+
+		} while (ans.equalsIgnoreCase("yes"));
+
+	}
+}
